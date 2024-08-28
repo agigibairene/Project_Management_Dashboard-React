d5495492d9487c55b2b76f12897279db2d78275a
agigibairene <i.agigiba@alustudent.com>
2024-08-28 15:32:45 +0200

    Project management dashboard

---
+++ Project_management/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject(){
+    setProjectsState(prevState =>{
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProject === null){
+    content = <NewProject /> 
+  }
+  else if (projectsState.selectedProject === undefined){
+    content = <NoProjectSelected  onStartAddProject={handleStartAddProject}/>;
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
+
+    </main>
+  );
+}
+
+export default App;
